e7ee7c09974d5af7fe84c115f4a101215c846099
Abdylbasit07 <zorobekovabdylbasit@gmail.com>
2022-12-15 00:59:18 +0600

    hw2

--- Store/urls.py
+++ Store/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import main_view
+from posts.views import main_view, products_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main_view)
+    path('', main_view),
+    path('products/', products_view)
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from posts.models import Product
 
 # Create your views here.
 
 def main_view(request):
     return render(request, 'layouts/index.html')
+
+def products_view(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        return render(request, 'products/products.html', context={
+            'products': products
+        })
